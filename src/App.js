9b73d9d92a2fc5b6158556faa7bff0cdae8adf1b
waseemAnsari11011 <waseemahm11011@gmail.com>
2025-09-25 16:05:26 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { CSpinner, useColorModes } from '@coreui/react'
 import './scss/style.scss'
 import { fetchVendorOrders } from './redux/actions/getAllOrdersAction'
 import { fetchVendorChatOrders } from './redux/actions/getNewChatOrdersAction'
+import { ToastContainer } from 'react-toastify' // 1. Import ToastContainer
+import 'react-toastify/dist/ReactToastify.css' // 2. Import the CSS
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
@@ -28,25 +30,25 @@ const App = () => {
 
   useEffect(() => {
     const fetchOrdersAndSetTheme = async () => {
-      await dispatch(fetchVendorOrders(vendorId));
-      await dispatch(fetchVendorChatOrders(vendorId));
+      await dispatch(fetchVendorOrders(vendorId))
+      await dispatch(fetchVendorChatOrders(vendorId))
 
-      const urlParams = new URLSearchParams(window.location.search);
-      const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0];
-      
+      const urlParams = new URLSearchParams(window.location.search)
+      const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
+
       if (theme) {
-        setColorMode(theme);
+        setColorMode(theme)
       }
 
       if (isColorModeSet()) {
-        return;
+        return
       }
 
-      setColorMode(storedTheme);
-    };
+      setColorMode(storedTheme)
+    }
 
-    fetchOrdersAndSetTheme();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    fetchOrdersAndSetTheme()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <HashRouter>
@@ -57,6 +59,7 @@ const App = () => {
           </div>
         }
       >
+        <ToastContainer />
         <Routes>
           <Route
             exact
--- src/redux/reducers/defaultReducers.js
+++ src/redux/reducers/defaultReducers.js
@@ -1,48 +1,56 @@
 // src/redux/reducers.js
 
-import { START_LOADING, STOP_LOADING, SET_USER, LOGOUT_USER, SET_IS_AUTHENTICATED, SET_TOKEN, SET_SIDEBAR_SHOW, SET_THEME } from '../actions/types';
+import {
+  START_LOADING,
+  STOP_LOADING,
+  SET_USER,
+  LOGOUT_USER,
+  SET_IS_AUTHENTICATED,
+  SET_TOKEN,
+  SET_SIDEBAR_SHOW,
+  SET_THEME,
+} from '../actions/types'
 
 const initialState = {
-    sidebarShow: true,
-    theme: 'light',
-    isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
-    loading: false,
-    user: JSON.parse(localStorage.getItem('user')) || {},
-    token: localStorage.getItem('token')
-};
-
-const changeState = (state = initialState, action) => {
-    const { type, ...rest } = action;
-    switch (type) {
-        case 'set':
-            return { ...state, ...rest };
-        case SET_SIDEBAR_SHOW:
-            return { ...state, sidebarShow: rest.sidebarShow };
-        case SET_THEME:
-            return { ...state, theme: rest.theme };
-        case SET_IS_AUTHENTICATED:
-            localStorage.setItem('isAuthenticated', rest.isAuthenticated);
-            return { ...state, isAuthenticated: rest.isAuthenticated };
-        case SET_TOKEN:
-            console.log("setToken is called")
-            localStorage.setItem('token', rest.token);
-            return { ...state, token: rest.token };
-        case START_LOADING:
-            return { ...state, loading: true };
-        case STOP_LOADING:
-            return { ...state, loading: false };
-        case SET_USER:
-            localStorage.setItem('user', JSON.stringify(rest.user));
-            localStorage.setItem('token', rest.user);
-            return { ...state, user: rest.user };
-        case LOGOUT_USER:
-            localStorage.removeItem('token');
-            localStorage.removeItem('user');
-            localStorage.setItem('isAuthenticated', 'false');
-            return { ...state, user: {}, isAuthenticated: false };
-        default:
-            return state;
-    }
+  sidebarShow: true,
+  theme: 'light',
+  isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
+  loading: false,
+  user: JSON.parse(localStorage.getItem('user')) || {},
+  token: localStorage.getItem('token'),
 }
 
-export default changeState;
+const changeState = (state = initialState, action) => {
+  const { type, ...rest } = action
+  switch (type) {
+    case 'set':
+      return { ...state, ...rest }
+    case SET_SIDEBAR_SHOW:
+      return { ...state, sidebarShow: rest.sidebarShow }
+    case SET_THEME:
+      return { ...state, theme: rest.theme }
+    case SET_IS_AUTHENTICATED:
+      localStorage.setItem('isAuthenticated', rest.isAuthenticated)
+      return { ...state, isAuthenticated: rest.isAuthenticated }
+    case SET_TOKEN:
+      console.log('setToken is called')
+      localStorage.setItem('token', rest.token)
+      return { ...state, token: rest.token }
+    case START_LOADING:
+      return { ...state, loading: true }
+    case STOP_LOADING:
+      return { ...state, loading: false }
+    case SET_USER:
+      localStorage.setItem('user', JSON.stringify(rest.user))
+      return { ...state, user: rest.user }
+    case LOGOUT_USER:
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+      localStorage.setItem('isAuthenticated', 'false')
+      return { ...state, user: {}, isAuthenticated: false }
+    default:
+      return state
+  }
+}
+
+export default changeState
